b1df4c9daf16d306b63f0b7fdd61553305d73a59
giovanni <giovannincs@hotmail.com>
2025-02-04 20:27:57 +0000

    correto

--- ex7.py
+++ ex7.py
@@ -2,15 +2,16 @@ distancia = float(input("Digite a distância percorrida (km): "))
 combustivel = float(input("Digite a quantidade de combustível consumido (litros): "))
 
 
-if combustivel == 0:
-    print("O consumo não pode ser calculado, pois a quantidade de combustível é zero.")
-consumo = distancia / combustivel
-    
-if consumo < 8:
-        mensagem = "Consumo alto! Melhor trocar de carro."
-elif 8 <= consumo <= 12:
-        mensagem = "Consumo moderado."
-else:
-        mensagem = "Consumo eficiente!"
-    
-print(f"O consumo foi de {consumo:} km/l. {mensagem}")
+if combustivel == 0 or distancia == 0:
+        print("O consumo não pode ser calculado, pois a quantidade de combustível ou a distância percorrida é zero.")
+else:       
+        consumo = distancia / combustivel
+                
+        if consumo < 8:
+                        mensagem = "Consumo alto! Melhor trocar de carro."
+        elif 8 <= consumo <= 12:
+                        mensagem = "Consumo moderado."
+        else:
+                        mensagem = "Consumo eficiente!"
+                
+        print(f"O consumo foi de {consumo} km/l. {mensagem}")
